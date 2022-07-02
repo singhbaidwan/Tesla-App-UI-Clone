ba29c02156791680dffa202ace13cb16758ce4bf
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-07-02 22:08:35 +0530

    Initial Commit

---
+++ My-Tesla/My_TeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TeslaApp.swift
+//  My-Tesla
+//
+//  Created by Dalveer singh on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct My_TeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
